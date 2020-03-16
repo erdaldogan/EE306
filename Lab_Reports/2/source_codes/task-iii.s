01fc1675c27f5aab652170ea899bf6c1251b9d03
erdal <erdalsidal99@gmail.com>
2020-03-16 20:02:45 +0300

    lab report 3 task 3

--- Lab_Reports/2/source_codes/task-iii.s
+++ Lab_Reports/2/source_codes/task-iii.s
@@ -1,2 +1,18 @@
-.global _start 
+.include    "address_map_arm.s"
+.text                       
+.global     _start
+
 _start: 
+        LDR R0, N // load the data word into R0
+        MOV R1, #0 // temp register
+        BL FINDSUM
+        MOV R0, R1 // move result from R1 to R0
+        B END
+FINDSUM:
+        ADD R1, R1, R0
+        SUBS R0, R0, #1 // count down
+        BXEQ LR // branch if 0
+        B FINDSUM    
+END:    B END  
+N:      .word 0x9
+.end
