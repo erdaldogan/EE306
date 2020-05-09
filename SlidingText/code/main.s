d9191f38a687d23dc14266d0dbe03aede1842187
erdal <erdalsidal99@gmail.com>
2020-05-09 15:14:44 +0300

    updates

--- SlidingText/code/main.s
+++ SlidingText/code/main.s
@@ -1,7 +1,7 @@
 .global _start
 _start:
     LDR R11, =0xFFFEC600 //timer adress
-    LDR R1, =2000000 // 200M
+    LDR R1, =200000000 // 200M
     STR R1, [R11] //set load register of the timer
     MOV R2, #0b011 //control configuration
     STR R2, [R11, #8] //set config bits
@@ -9,28 +9,34 @@ _start:
     /* AT THIS POINT WE CAN USE R1 & R2 REGISTERS AS WE WISH */
     /* DON'T FORGET R11 & R12 ARE RESERVED FOR TIMER OPERATION */
     
-    LDR R0, =0xFF200020 // 7-segment adress
+    LDR R0, =0xFF200024 // 7-segment adress
     LDR R1, =ALPHABET // address of the character set
     LDR R2, =INPUT // user input
-    LDR R3, =6
-    LDRB R4, [R2] // first letter of the user input
-    SUB R4, R4, #65 // get the location of the letter in the char set
-    
-    /* get the 7-segment equivalent of the character */
-    LDRB R4, [R1, R4]
-    STRB R4, [R0, #1]
-    B END
-    
-    
+	MOV R3, #0 //count
+	B PRINT_STRING
+    	
+PRINT_STRING:
+	LDRB R4, [R2, R3] // ascii code of the character
+	CMP R4, #00
+	BEQ END
+	SUB R4, R4, #65 // location of that character in the representation array
+	LDRB R4, [R1, R4] // retrieve the character representation itself
+	STRB R4, [R0] // write on to the 7-segment
+	SUB R0, R0, #0x01
+	ADD R3, R3, #1
+	BL DELAY
+	B PRINT_STRING
+	
+  
 DELAY:
     LDR R12, [R11, #0xC]
     CMP R12, #1
     BNE DELAY
     STR R12, [R11, #0xC] // reset status flag.
     BX LR
-    
+	
 END: B END
-INPUT: .asciz "ABCDEF"
+INPUT: .asciz "ABCDEF" //.asciz appends a zero at the end of the string as an finish indicator
 ALPHABET: .byte 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71, 0x7D, 0x76, 0x06, 0x0E, 0x75, 0x38, 0x15, 0x54, 0x5C, 0x73, 0x67, 0x50, 0x6D, 0x78, 0x3E, 0x62, 0x6A, 0x64, 0x6E, 0x5B
 // 1 A = 0x77
 // 2 B = 0x7C
@@ -59,3 +65,5 @@ ALPHABET: .byte 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71, 0x7D, 0x76, 0x06, 0x0E, 0x75
 // 25 Y = 0x6E
 // 26 Z = 0x5B
 .end
+
+
